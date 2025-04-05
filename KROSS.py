11992b1dfabcbe1221d1d6682701e1e6d91dfb8e
TotallyMew <augustas.koko@gmail.com>
2025-04-05 13:08:10 +0300

    Atnaujinta scrape funkcija: abidvi spalvos gaunamos

--- KROSS.py
+++ KROSS.py
@@ -13,12 +13,13 @@ def scrapeAndTranslateToFileKROSS(url, output_file):
     key_translations = loadTranslations(resource_path("vertimasDetalesPL-LT.txt"))
     value_translations = loadValueTranslations(resource_path("vertimasSavybesPL-LT.txt"))
 
+    count = 0
     all_data = []
     unique_keys = set()
 
     try:
         response = requests.get(url)
-        response.raise_for_status()  # HTTP error check
+        response.raise_for_status()
 
         soup = BeautifulSoup(response.text, "html.parser")
         additional_section = soup.find("div", id="additional")
@@ -36,20 +37,44 @@ def scrapeAndTranslateToFileKROSS(url, output_file):
         for table in tables:
             rows = table.find_all("tr")
             table_data = {}
-            
+
             for row in rows:
                 cells = row.find_all("td")
                 if len(cells) >= 2:
                     key = cells[0].get_text(strip=True).title()
                     value = cells[1].get_text(strip=True)
-                    
+
                     if value.upper() != "BRAK":
+                        if key.upper() == "KOLOR BAZOWY":
+                            continue
                         translated_key = key_translations.get(key, key)
                         translated_value = value_translations.get(value, value)
                         translated_value = verstTikPirmaZodi(translated_value, value_translations)
-                        
+
                         table_data[translated_key] = translated_value
                         unique_keys.add(translated_key)
+
+            # Add translated color data (main and secondary)
+            if count == 0:
+                color_block = soup.select_one(".product-related-colors .block-title.title")
+                count+= 1
+                if color_block:
+                    color_text = color_block.get_text(strip=True).replace("Kolor bazowy:", "").strip()
+                    colors = color_text.split()
+
+                    if len(colors) >= 1:
+                        main_color = colors[0]
+                        main_color_translated = value_translations.get(main_color, main_color)
+                        main_color_translated = verstTikPirmaZodi(main_color_translated, value_translations)
+                        table_data["Pagrindinė spalva"] = main_color_translated
+                        unique_keys.add("Pagrindinė spalva")
+
+                    if len(colors) >= 2:
+                        secondary_color = colors[1]
+                        secondary_color_translated = value_translations.get(secondary_color, secondary_color)
+                        secondary_color_translated = verstTikPirmaZodi(secondary_color_translated, value_translations)
+                        table_data["Papildoma spalva (antra)"] = secondary_color_translated
+                        unique_keys.add("Papildoma spalva (antra)")
 
             all_data.append(table_data)
 
@@ -68,7 +93,8 @@ def scrapeAndTranslateToFileKROSS(url, output_file):
         print(f"Error: {e}")
         return f"Nepavyko apdoroti duomenų dėl klaidos: {e}"
 
-    return f"Total unique keys: {uniqueKeysTotal}"  
+    return f"Total unique keys: {uniqueKeysTotal}"
+
  
 
 def automatizacija(
